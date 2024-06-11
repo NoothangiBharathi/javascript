7db0610ca40f544f323812a5b8b6cdbd2102b2bf
NoothangiBharathi <noothangi.bharathi1999@gmail.com>
2024-06-11 13:45:05 +0530

    javascript

---
+++ index.js
@@ -0,0 +1,53 @@
+ /*let age=25;
+ let salary=10000;
+  let gst=1000;
+console.log(typeof gst)
+  console.log(`you are ${age}years old`);
+  console.log(`you have ${salary} salary is good`)
+  console.log(`you have ${gst} gst right!`);
+
+
+  let name="bharathi";
+  let emailid="bro@12345";
+  let dressprice="123456789";
+  console.log("typeof name");
+  console.log("your sister name is:${name}");
+  console.log("your emailid is:${emailid}");
+  console.log("your dress price is:${dressprice}");
+
+
+  let good=true;
+  let isstudent=true;
+  let isyourhealth=true;
+  console.log(`you are beautiful:${good}`);
+  console.log(`are you a student:${isstudent}`);
+  console.log(`is your health ok:${isyourhealth}`);*/
+
+  
+   /*let student=30;
+   student=student+2;
+  student=student-2;
+  student=student/2;
+    student=student**2;
+   student=student%2;
+   student=student/2
+   let extrastudent=student%3;
+  student=student+=2;
+  student--;
+
+
+  let result=1*4**2+1-2;
+   console.log(result);*/
+
+   //accept out in easy way
+  /*let username;
+  username=window.prompt("your user name");
+  console.log(username);*/
+
+
+ let username;
+ document.getElementById("my submit").onclick=function(){
+  username=Document.getElementById("text").value;
+  document.getElementById("myh1").textContent='hello{username}'
+console.log(username);
+ }
